72a029a19edb34fb51601356df163b4609dda3ac
psyklohne <logan.duer@gmail.com>
2025-12-16 17:15:06 -0800

    Create Administrator class for system management
    Added Administrator class with full permissions for system management.

--- Administrator.java
+++ Administrator.java
@@ -1,1 +1,27 @@
-
+/**
+ * Administrator user with full permissions for system management
+ */
+public class Administrator extends User {
+    // Constructor calls parent User class with provided username
+    public Administrator(String username) { 
+        super(username); 
+    }
+    
+    @Override
+    public boolean canManageResources() { 
+        // Administrators can always manage resources (add/edit/remove)
+        return true;
+    }
+    
+    @Override
+    public String getUserRole() { 
+        // Returns the role name for display purposes
+        return "Administrator"; 
+    }
+    
+    @Override
+    public String toString() { 
+        // Provides formatted string representation for printing
+        return "ADMIN: " + getUsername() + " [ID: " + getUserId() + "]"; 
+    }
+}
