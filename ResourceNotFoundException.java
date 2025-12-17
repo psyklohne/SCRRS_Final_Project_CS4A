689a937489b33f813e8a830dce39a1f59a2ef753
psyklohne <logan.duer@gmail.com>
2025-12-16 17:26:14 -0800

    Add ResourceNotFoundException class

---
+++ ResourceNotFoundException.java
@@ -0,0 +1,4 @@
+// Exception thrown when requested resource cannot be found
+public class ResourceNotFoundException extends Exception {
+    public ResourceNotFoundException(String message) { super(message); }
+}
