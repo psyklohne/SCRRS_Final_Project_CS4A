0217541b1c742f7b88fff003b04e0827d161946b
psyklohne <logan.duer@gmail.com>
2025-12-16 17:24:11 -0800

    Create InvalidTimeSlotException class
    Exception thrown for invalid time slot selections (out of range)

---
+++ InvalidTimeSlotException.java
@@ -0,0 +1,4 @@
+// Exception thrown for invalid time slot selections (out of range)
+public class InvalidTimeSlotException extends Exception {
+    public InvalidTimeSlotException(String message) { super(message); }
+}
