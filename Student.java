b51dac8a500a270e80a818bb412ba49ec7024789
psyklohne <logan.duer@gmail.com>
2025-12-16 17:19:56 -0800

    Implement Student class extending User
    Added Student class with limited permissions and role methods.

--- Student.java
+++ Student.java
@@ -1,1 +1,28 @@
-
+/**
+ * Student user with limited permissions
+ * Can only view resources and manage own reservations
+ */
+public class Student extends User {
+    // Constructor calls parent User class with provided username
+    public Student(String username) { 
+        super(username); 
+    }
+    
+    @Override
+    // Students cannot manage resources (add/edit/remove)
+    public boolean canManageResources() { 
+        return false;
+    }
+    
+    @Override
+    // Returns the role name for display purposes
+    public String getUserRole() { 
+        return "Student"; 
+    }
+    
+    @Override
+    // Provides formatted string representation for printing
+    public String toString() { 
+        return "STUDENT: " + getUsername() + " [ID: " + getUserId() + "]"; 
+    }
+}
